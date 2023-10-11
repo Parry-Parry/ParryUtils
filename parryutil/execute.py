b047c90b7b8aff8e0ed4553894248c34030af8b3
Andrew Parry <2393265p@student.gla.ac.uk>
2023-10-11 11:54:13 +0100

    refactor of train script

--- parryutil/execute.py
+++ parryutil/execute.py
@@ -8,19 +8,19 @@ import json
 import logging 
 import subprocess as sp
 
-def main(config_path : str):
-    runs = load(open(config_path, 'r'), Loader=Loader)
+def execute(config_path : str):
+    executions = load(open(config_path, 'r'), Loader=Loader)
 
-    for k, cfg in runs.items():
-        logging.info('\n'.join([f'RUN NAME: {k}', 'ARGS:', json.dumps(cfg['args'], indent=2)]))
+    for k, cfg in executions.items():
+        logging.info('\n'.join([f'EXECUTION NAME: {k}', 'ARGS:', json.dumps(cfg['args'], indent=2)]))
         cmd = ['python', '-m', cfg['script']]
         for arg, val in cfg['args'].items():
             cmd.append(f'--{arg}')
             cmd.append(str(val))
         sp.run(cmd)
     
-    return f'Completed {len(runs)} runs.'
+    return f'Completed {len(executions)} executions.'
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    Fire(main)
+    Fire(execute)
