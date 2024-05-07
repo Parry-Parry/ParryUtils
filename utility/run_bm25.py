d184c85926c256a010070639f1f74a2549852291
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-05-07 16:18:04 +0100

    flexible bm25

--- utility/run_bm25.py
+++ utility/run_bm25.py
@@ -15,13 +15,15 @@ def main(ir_dataset : str,
          out_path : str,
          num_threads : int = 8,
          budget : int = 100,
-         subset : int = -1) -> str:
+         subset : int = -1,
+         k1 : float = 1.2,
+         b : float = 0.75) -> str:
     
     dataset = irds.load(ir_dataset)
     queries = pd.DataFrame(dataset.queries_iter()).rename(columns={'query_id': 'qid', 'text': 'query'})
     if subset > 0: queries = queries.sample(n=subset)
     index = PisaIndex(index_path, threads=num_threads)
-    bm25 = index.bm25(k1=1.2, b=0.75, num_results=budget, verbose=True)
+    bm25 = index.bm25(k1=k1, b=b, num_results=budget, verbose=True)
     print(f'Running BM25 on {len(queries)} queries')
 
     result = bm25.transform(queries)
