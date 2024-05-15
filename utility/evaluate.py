5d15b47c79eff033d3716ace6ebb1197d1cc291f
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-05-15 11:26:36 +0100

    add eval func to simplify things

---
+++ utility/evaluate.py
@@ -0,0 +1,45 @@
+from fire import Fire 
+import os 
+from os.path import join
+from ir_measures import evaluator, read_trec_run 
+from ir_measures import *
+import ir_datasets as irds
+import pandas as pd
+
+def main(eval :str, run_dir : str, out_dir : str, rel : int = 1, iter=False):
+    parent_dir = os.path.dirname(run_dir)
+    os.makedirs(parent_dir, exist_ok=True)
+    files = [f for f in os.listdir(run_dir) if os.path.isfile(join(run_dir, f))]
+    ds = irds.load(eval)
+    qrels = ds.qrels_iter()
+    metrics = [AP(rel=rel), NDCG(cutoff=10), NDCG(cutoff=5), NDCG(cutoff=1), R(rel=rel)@100, R(rel=rel)@1000, P(rel=rel, cutoff=10), RR(rel=rel), RR(rel=rel, cutoff=10)]
+    evaluate = evaluator(metrics, qrels)
+    df = []
+    for file in files:
+        if file.endswith(".gz"):
+            name = file.strip('.gz')
+            run = read_trec_run(join(run_dir, file))
+            if iter:
+                res = {}
+                for elt in evaluate.calc_iter(run):
+                    df.append(
+                        {
+                            'name' : name,
+                            'query_id' : elt.query_id,
+                            'metric' : str(elt.measure),
+                            'value' : elt.value,
+                        }
+                    )
+            else:
+                res = evaluate.calc_aggregate(run)
+                res = {str(k) : v for k, v in res.items()}
+                res['name'] = name 
+                df.append(res)
+    
+    df = pd.DataFrame.from_records(df)
+    df.to_csv(out_dir, sep='\t', index=False)
+
+    return "Success!"
+
+if __name__ == '__main__':
+    Fire(main)
